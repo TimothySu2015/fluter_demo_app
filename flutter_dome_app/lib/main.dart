9f934e1c977637d26965e1ed6cebaaa62b26253c
DESKTOP-UVAC77F\TimothySu <timothy.su@thinkpower.com.tw>
2020-03-26 02:39:31 +0800

    fix bloc

--- flutter_dome_app/lib/main.dart
+++ flutter_dome_app/lib/main.dart
@@ -2,24 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterdomeapp/view_models/login/bloc/bloc.dart';
 
+import 'pages/home/home.dart';
+import 'view_models/login/bloc/bloc.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,15 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,19 +31,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  final LoginBloc _loginBloc = LoginBloc();
   final account = TextEditingController();
   final password = TextEditingController();
-
-  void _incrementCounter() {
-    _loginBloc
-        .add(LoginCredentials(account: account.text, password: password.text));
-
-    _counter++;
-
-    // loginBloc.close();
-    //  print(loginBloc.initialState);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,51 +40,72 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocBuilder<LoginBloc, LoginState>(
-        bloc: _loginBloc,
-        builder: (BuildContext context, LoginState state) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
+      body: BlocProvider(
+          create: (context) => LoginBloc(),
+          child: BlocBuilder<LoginBloc, LoginState>(
+            builder: (BuildContext context, LoginState state) {
+              print('state change');
+
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text('account:'),
-                    Container(
-                      width: 100, // do it in both Container
-                      child: TextField(
-                        controller: account,
+                    Row(
+                      children: <Widget>[
+                        Text('account:'),
+                        Container(
+                          width: 100, // do it in both Container
+                          child: TextField(
+                            controller: account,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Text('password:'),
+                        Container(
+                          width: 100, // do it in both Container
+                          child: TextField(
+                            controller: password,
+                            obscureText: true,
+                          ),
+                        )
+                      ],
+                    ),
+                    Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.display1,
+                    ),
+                    Text(state.toString()),
+                    SizedBox(
+                      width: 100.0,
+                      child: RaisedButton(
+                        onPressed: () {
+                          _counter++;
+                          BlocProvider.of<LoginBloc>(context).add(
+                              LoginCredentials(
+                                  account: account.text,
+                                  password: password.text));
+                        },
+                        child: Text("test"),
                       ),
                     )
                   ],
                 ),
-                Row(
-                  children: <Widget>[
-                    Text('password:'),
-                    Container(
-                      width: 100, // do it in both Container
-                      child: TextField(
-                        controller: password,
-                        obscureText: true,
-                      ),
-                    )
-                  ],
-                ),
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-                Text(state.toString())
-              ],
-            ),
-          );
-        },
-      ),
+              );
+            },
+          )),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          print('click');
+          _counter++;
+          BlocProvider.of<LoginBloc>(context).add(
+              LoginCredentials(account: account.text, password: password.text));
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
